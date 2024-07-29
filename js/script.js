9df7a4c0e7808fa83291e369dd018eff15601d99
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-07-29 08:05:13 +0530

    home page js file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-// showing the sidebar
-function showSidebar(){
-    const sidebar = document.querySelector('.side')
-    sidebar.style.display = 'flex'
-}
-
-
-// hiding the sidebar
-function hideSidebar(){
-    const hidesidebar = document.querySelector('.side')
-    hidesidebar.style.display = 'none'
+// Show the sidebar
+const showSidebar = () => {
+    const sidebar = document.querySelector('.side');
+    sidebar.style.display = 'flex';
 };
 
-// preloader - hiding the preloader 
-var loader = document.getElementById("preloader");
+// Hide the sidebar
+const hideSidebar = () => {
+    const hidesidebar = document.querySelector('.side');
+    hidesidebar.style.display = 'none';
+};
 
-window.addEventListener("load", function(){
-    loader.style.display = "none"
+// Preloader - hiding the preloader
+const loader = document.getElementById("preloader");
+
+window.addEventListener("load", () => {
+    loader.style.display = "none";
 });
+
+// sending email when contact form submitted
+const submitForm = () => {
+    const email = document.getElementById("email").value;
+    const body = 'This is the Body'; 
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "shuvamsaha2000@gmail.com",
+        Password : "779B5104FB65FF4056DCB9D7DD31D67138CD",
+        To : 'shuvamsaha2000@gmail.com',
+        From : email,
+        Subject : "New Contact Form Enquiry",
+        Body : body
+    }).then(
+      message => confirm(message)
+    ).catch((error) => {
+        console.log("Failed To Send Email", error)
+        alert("Failed to send Email")
+    })
+}
